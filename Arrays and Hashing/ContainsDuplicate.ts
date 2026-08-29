ddfecd5d5a6b9e052451115b7ff99afe9fe27cb1
Jestony Silvano <silvanojestony27@gmail.com>
2026-08-29 23:34:29 +0800

    Improve efficiency of twoSum and ContainsDuplicate

--- Arrays and Hashing/ContainsDuplicate.ts
+++ Arrays and Hashing/ContainsDuplicate.ts
@@ -24,19 +24,37 @@ Constraints:
 
 */
 
-const containsDuplicate = (nums: number[]): boolean => {
-  for (let i = 0; i < nums.length; i++ ) {
-    let a = nums[i]
-    for (let x = i+1; x < nums.length; x++ ) {
-      let b = nums[x]
-      if (a === b) return true;
-    }
-  }
-  return false;
-}
-
-console.log(containsDuplicate([1, 2, 3, 4, 5, 3]));
+// const containsDuplicate = (nums: number[]): boolean => {
+//   for (let i = 0; i < nums.length; i++ ) {
+//     let a = nums[i]
+//     for (let x = i+1; x < nums.length; x++ ) {
+//       let b = nums[x]
+//       if (a === b) return true;
+//     }
+//   }
+//   return false;
+// }
 
 //This solution is a bruteforce search and worst type of solution O(n²)
 //For now, this is the solution i came up with
 //I will improve my solutions as I progress further with my checklists
+
+const containsDuplicate = (nums: number[]): boolean => {
+  const map = new Map<number, number>();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (!map.has(nums[i]!)){
+      map.set(nums[i]!, map.get(nums[i]!)! + 1)
+    } else {
+      return true;
+    }
+  }
+  return false;
+}
+//After a few days of learning dsa, I tried to go back here
+//See if i can solve it in my own with more efficient
+//Did it in my first try
+
+console.log(containsDuplicate([1, 2, 3, 4, 5, 3]));
+console.log(containsDuplicate([1, 2, 3, 4]));
+
--- Arrays and Hashing/twoSum.ts
+++ Arrays and Hashing/twoSum.ts
@@ -7,25 +7,45 @@
 // nums = [2, 7, 11, 15]
 // target = 9
 
-
 // Output:
 // [0, 1]
+
 // Constraints
 // Each input has exactly one solution.
 // You may not use the same element twice.
 // Return the indices in any order.
+
+// const twoSum = (nums: number[], target: number): number[] => {
+//   for (let i = 0;  i < nums.length; i++) {
+//     for (let x = 0; x < nums.length; x++) {
+//       if ((nums[i]! + nums[x]!) === target) {
+//         return [i, x];
+//       }
+//     }
+//   }
+//   return [];
+// }
+
+//Been using brute force so i tried to study about whats a better  algorithm for this
+//I have learned that I should use hashmap, so in the next problems. I will be using hashmap
+
 const twoSum = (nums: number[], target: number): number[] => {
+  const map = new Map<number, number>();
   for (let i = 0;  i < nums.length; i++) {
-    for (let x = 0; x < nums.length; x++) {
-      if ((nums[i]! + nums[x]!) === target) {
-        return [i, x];
-      }
+    const find = target - nums[i]!;
+    if (!map.has(find)) {
+      map.set(nums[i]!, i)
+    } else {
+      return [map.get(find)!, i]
     }
   }
   return [];
 }
 
-console.log(twoSum([2, 7, 11, 15], 9));
+//I tried it again but this time no help
+//Successfully did it in first try
 
-//Been using brute force so i tried to study about whats a better  algorithm for this
-//I have learned that I should use hashmap, so in the next problems. I will be using hashmap
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([2, 4, 6,  7, 11, 15], 13));
+
